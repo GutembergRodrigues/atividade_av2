2d1979e552cfaea09e2271f113a4c38217991f82
Nicollas <n1coll4s@outlook.com>
2021-05-14 14:32:27 -0300

    Adicionando metodo area triangulo

---
+++ src/br/edu/fafic/es/main/Triangulo.java
@@ -0,0 +1,8 @@
+package br.edu.fafic.es.main;
+
+public class Truangulo {
+    public void trianguloArea(float b, float h){
+        float area = (b * h) / 2;
+        System.out.println("Area do Triangulo: " + area);
+    }
+}
