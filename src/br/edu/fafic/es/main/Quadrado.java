10236cbc30fe52ca471fb0fa62f33097292ec0ad
Gutemberg Rodrigues <gutemberghoracio@hotmail.com>
2021-05-14 14:33:50 -0300

    Adicionando metodo area do quadrado

---
+++ src/br/edu/fafic/es/main/Quadrado.java
@@ -0,0 +1,20 @@
+package br.edu.fafic.es.main;
+
+
+public class Quadrado {
+
+    
+    public static void main(String[] args) {
+        
+        double base, altura, areaRetangulo;
+        
+        base = 10.34;
+        altura = 14.54;
+        areaRetangulo = base * altura;
+        
+        System.out.println("A area de um retangulo de base = "  + base);
+        System.out.println("e altura " + altura);
+        System.out.println("é" + areaRetangulo);
+    }
+    
+}
