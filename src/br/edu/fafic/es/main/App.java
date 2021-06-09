b42cda57d18137657db560485a0e4174a5743cdc
Nicollas Estrela Guedes <n1coll4s@outlook.com>
2021-06-08 22:21:22 -0300

    Update App.java

--- src/br/edu/fafic/es/main/App.java
+++ src/br/edu/fafic/es/main/App.java
@@ -1,7 +1,7 @@
 
 public class App {
     public static void main(String[] args) {
-        Triangulo triangulo = new Triangulo("dale");
+        Triangulo triangulo = new Triangulo();
 	triangulo.trianguloArea(10,5)
 	
     }
